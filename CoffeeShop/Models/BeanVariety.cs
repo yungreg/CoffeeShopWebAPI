feb8ad43305ce4c2a7252a6129568a388e074b42
Reginald Williams Jr <rlwjr433@gmail.com>
2023-01-18 10:16:53 -0600

    initial commmit- CoffeeRepo in progress

---
+++ CoffeeShop/Models/BeanVariety.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShop.Models
+{
+    public class BeanVariety
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(255, MinimumLength = 3)]
+        public string Region { get; set; }
+        //Notes is not required, wehich is why ID 1 can be null
+        public string Notes { get; set; }
+    }
+}
---
+++ js-app/main.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+const url = "https://localhost:5001/api/beanvariety/";
+
+const button = document.querySelector("#run-button");
+button.addEventListener("click", () => {
+    getAllBeanVarieties()
+        .then(beanVarieties => {
+            console.log(beanVarieties);
+        })
+});
+
+function getAllBeanVarieties() {
+    return fetch(url).then(resp => resp.json());
+}
